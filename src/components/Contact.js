06130496d130c953e6eb1354ad92b6cb1f36c3f9
Ali Shahal <alishahal9656@gmail.com>
2025-08-23 01:14:00 +0530

    ui changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -77,7 +77,7 @@ const Contact = () => {
               viewport={{ once: true }}
               className="flex items-start space-x-4"
             >
-              <div className="w-12 h-12 bg-orange-500 rounded-lg flex items-center justify-center flex-shrink-0">
+              <div className="w-12 h-12 bg-blue-500 rounded-lg flex items-center justify-center flex-shrink-0">
                 <svg
                   className="w-6 h-6 text-white"
                   fill="none"
